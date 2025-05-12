71c0542b1f62a54c4caec679b1a2462244acd3fa
Jabi <michal.jabi@gmail.com>
2025-05-12 13:37:37 +0200

    ch(a20): make it work + make it right... import / export variants.

--- jsajx/src/A-the-modular-js/20-import-export-variants/a20-challenge-project/czesuaf-util.js
+++ jsajx/src/A-the-modular-js/20-import-export-variants/a20-challenge-project/czesuaf-util.js
@@ -1,1 +1,25 @@
+import { addNumbers, averageFrom } from "./main/helpers/my-things.js"
+import { firstLetterToUpper as capitalize } from "./main/tools/tool-1.js"
+import { countWords } from "./main/tools/tool-2.js"
+import someUtil, { toSecondPower } from './main/utils/some-util.js'
 
+/*
+const lastLetter = someUtil.lastLetter
+const numberOfChars = someUtil.numberOfChars
+*/
+const {lastLetter, numberOfChars} = someUtil; // object destructuring
+
+export const stringHelper = {
+    lastLetter,
+    numberOfChars,
+    countWords,
+    capitalize,
+}
+
+export const numberHelper = {
+    toSecondPower,
+    addNumbers,
+    averageFrom
+}
+
+//console.log(numberHelper)
