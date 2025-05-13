1765a4c068e7853a337cd4e4b9fb79846aead654
Jabi <michal.jabi@gmail.com>
2025-05-13 11:53:52 +0200

    explain(d10): filter, map, reduce + use cases.

--- jsajx/src/D-functional-programming/10-filter-map-reduce/d10-2nd-filter-map-reduce-explain.js
+++ jsajx/src/D-functional-programming/10-filter-map-reduce/d10-2nd-filter-map-reduce-explain.js
@@ -10,30 +10,69 @@
 // Niewątpliwą zaletą metod: map / filter / reduce - jest możliwość ich "chain'owania" - łączenia
 // i wywoływania jedna po drugiej.
 
+const ref = (input, input2, i3, i4) => 'Hello';
+const ref2 = () => {
+
+
+
+	return 'Hello';
+}
+
+console.log(ref instanceof Function)
+console.log(ref instanceof Object)
+
+console.log(ref())
+console.log(ref2())
+
 // Mapowanie
 // Transformacja elementów
-const mapped = [1,2,3,4,5].map(nr => 'Chapter #' + nr);
+const mapped = [0, 1, 2, 3, 4, 5].filter(Boolean).map(nr => { return 'Chapter #' + nr });
 console.log(mapped);
 
 // Filtrowanie
 // Zmiana ilości elementów
-const oddNumbers = [1,2,3,4,5].filter(nr => nr % 2);
+
+// Falsy
+console.log(Boolean(''))
+// Truthy
+console.log(Boolean([]))
+console.log(Boolean({}))
+console.log(Boolean(new Date()))
+
+console.log(Boolean(10 % 2))
+console.log(!!(111 % 2))
+console.log(+'123')
+console.log(Number('123'))
+
+let value = NaN;
+
+if (value || value === 0) {
+	console.log('?')
+}
+
+console.log(11 % 2)
+console.log(12 % 2)
+
+const oddNumbers = [1, 2, 3, 4, 5].filter((_, idx)=> idx % 2);
 console.log(oddNumbers);
 
 // "Redukcja" elementów
 // Gdzie map i filter nie może tam reduce pośle!
-const sumOfElements = [1,2,3,4,5].reduce((acc, value) => acc + value);
+const sumOfElements = [1, 2, 3, 4, 5].reduce((acc, value) => {
+	acc.cash += value;
+	return acc
+}, { cash: 0 });
 console.log(sumOfElements);
 
 // Rozważmy następujący przykład:
 // Podsumowanie wartości wszystkich operacji uznania na koncie:
 
 const cashBalance = [
-	{id: 5, cash: 100, type: 'IN'},
-	{id: 4, cash: -300, type: 'OUT'},
-	{id: 3, cash: -1200, type: 'OUT'},
-	{id: 2, cash: 400, type: 'IN'},
-	{id: 1, cash: 2000, type: 'IN'},
+	{ id: 5, cash: 100, type: 'IN' },
+	{ id: 4, cash: -300, type: 'OUT' },
+	{ id: 3, cash: -1200, type: 'OUT' },
+	{ id: 2, cash: 400, type: 'IN' },
+	{ id: 1, cash: 2000, type: 'IN' },
 ]
 
 const inCash = cashBalance.filter((operation) => operation.type === 'IN').map(o => o.cash).reduce((a, b) => a + b);
@@ -42,9 +81,12 @@ console.log(inCash);
 // Korzystając z bardziej funkcyjnego podejścia można to zapisać bardziej przejrzyście:
 
 const operationTypeIn = o => o.type === 'IN';
-const pluckCash = o => o.cash;
+export const pluckCash = o => o.cash;
 const sum = (a, b) => a + b;
-console.log( cashBalance.filter(operationTypeIn).map(pluckCash).reduce(sum) );
+
+pluckCash({ cash: 3020 }) //=
+
+console.log(cashBalance.filter(operationTypeIn).map(pluckCash).reduce(sum));
 
 // Teraz nasze małe pomocnicze funkcje są re-używalne, możemy szybko policzyć aktualny stan konta
 const currentBalance = cashBalance.map(pluckCash).reduce(sum);
@@ -52,15 +94,15 @@ console.log(currentBalance);
 
 // Dodając dodatkowy filtr - możemy policzyć obciążenia:
 const operationTypeOut = o => o.type === 'OUT';
-console.log( cashBalance.filter(operationTypeOut).map(pluckCash).reduce(sum) );
+console.log(cashBalance.filter(operationTypeOut).map(pluckCash).reduce(sum));
 
 // Ilość przykładów można mnożyć:
 // 1 Policz ilość wszystkich operacji IN:
-console.log( cashBalance.filter(operationTypeIn).length )
+console.log(cashBalance.filter(operationTypeIn).length)
 
 // 2 Policz ilość wszystkich operacji OUT:
-console.log( cashBalance.filter(operationTypeOut).length )
+console.log(cashBalance.filter(operationTypeOut).length)
 
 // 3 Odtwórz stan konta po 3 operacji:
 const idLowerOrEqual3 = o => o.id <= 3;
-console.log( cashBalance.filter(idLowerOrEqual3).map(pluckCash).reduce(sum) )
+console.log(cashBalance.filter(idLowerOrEqual3).map(pluckCash).reduce(sum))
