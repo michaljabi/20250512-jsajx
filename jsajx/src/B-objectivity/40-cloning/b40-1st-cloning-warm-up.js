450abb49c95028d1fbde12d858a6a828778562e5
Jabi <michal.jabi@gmail.com>
2025-05-14 14:59:03 +0200

    wp(b40): solved

--- jsajx/src/B-objectivity/40-cloning/b40-1st-cloning-warm-up.js
+++ jsajx/src/B-objectivity/40-cloning/b40-1st-cloning-warm-up.js
@@ -1,4 +1,5 @@
 import { assertThat } from '../../j4b1-assert.js'
+// import _ from 'lodash'
 /**
  * b40-cloning
  * Warm up
@@ -26,10 +27,56 @@ const myFirstImpressiveObject = {
 	}
 }
 
+console.log(typeof 0)
+console.log(typeof '')
+console.log(typeof false)
+console.log(typeof (() => { }))
+
+console.log(typeof {})
+console.log(typeof new Object())
+console.log(typeof new Date())
+console.log(typeof [])
+
+
+console.log(typeof null);
+
+const value = null;
+
+console.log(value === null);
+
+
+// 2 rzecz nie do pogodzenia:
+console.log(NaN === NaN);
+
+// kontra to: (mogło by tak być :( )
+console.log(Infinity === Infinity);
+console.log(1000 / 0);
+
+// więc radzimy sobie tak:
+console.log(Number.isNaN(NaN))
+
+console.log(Number('2ddwdq'))
+console.log(784 / '2ddwdq')
+
 function cloner(objectToClone) {
 	// #Reguła:
 	// Kodzik można pisać tylko w tym bloku.
-	return objectToClone;
+	// return Object.assign({}, objectToClone);
+
+	// return _.cloneDeep(objectToClone);
+	const clone = { ...objectToClone };
+
+	// console.log(Object.keys(objectToClone))
+	for (const property in objectToClone) {
+		// console.log(property)
+		// console.log(objectToClone[property])
+		const value = objectToClone[property];
+		if (value !== null && typeof value === 'object') {
+			clone[property] = { ...value }
+		}
+	}
+
+	return clone;
 }
 
 
